53f83181abc3f69ec86b37761c33e1b42501017c
saeki4n <saeki.oshigoto@gmail.com>
2025-05-08 22:00:55 +0900

    現在時刻と開始するボタン表示

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,23 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Start Screen')));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Edit Snap'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'こんにちは！\n今日は ${DateTime.now()} です。',
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(child: const Text('開始する'), onPressed: () {}),
+          ],
+        ),
+      ),
+    );
   }
 }
