320dd9683f0ef2ac44087d8f0fe61c90bf3bc225
saeki4n <saeki.oshigoto@gmail.com>
2025-05-09 23:54:38 +0900

    ndkVersionの修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 }
 
 android {
+
     namespace = "com.example.edit_snap"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
